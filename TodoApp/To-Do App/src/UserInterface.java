c0988b86863439351d1ce75c1089da1508603a31
D-o-t-C-o-m <valkuros@gmail.com>
2024-12-06 17:58:00 -0500

    clarified text in remove & mark, added a validation that entry is a number, and added a way to cancel task addition

--- TodoApp/To-Do App/src/UserInterface.java
+++ TodoApp/To-Do App/src/UserInterface.java
@@ -12,38 +12,71 @@ public UserInterface(TodoList todoList, Scanner scan) {
 public void start() {
 	System.out.println("===To Deux===");
 	System.out.println("(H)elp for commands");
+
 	while (true) {
+
 		System.out.print("Command: ");
 		String command = scanner.nextLine().toLowerCase();
-		if (command.equals("stop") || command.equals("quit") || command.equals("q")) {
+
+		if (command.equals("stop") || command.equals("quit") || command.equals("q") || command.equals("exit")) {
 			break;
 
 		} else if (command.equals("add") || command.equals("a")) {
-			System.out.print("Task to add: ");
+			System.out.print("Task to add (!B to go cancel): ");
 			String task = scanner.nextLine();
-			this.todoList.add(task);
+			if (task.equals("!B") || task.equals("!b")) {
+				System.out.println();
+			} else {
+				this.todoList.add(task);
+			}
 
 		} else if (command.equals("export") || command.equals("e")) {
 			Utility utility = new Utility();
 			utility.createFile();
 
 	}
+
 	else if (command.equals("list") || command.equals("l")) {
 			if(todoList.size() == 0){
 				System.out.println("There are no tasks! (A)dd one now?");
 			} else {
 				todoList.print();
 			}
+
 		} else if (command.equals("mark") || command.equals("m")) {
 				todoList.print();
-			System.out.print("Item to mark complete: ");
-			int number = Integer.valueOf(scanner.nextLine());
+			int number = 0;
+			if(scanner.hasNextInt()) {
+				number = scanner.nextInt();
+			} else {
+				System.out.println("Please enter a number");
+				scanner.nextLine();
+				System.out.println();
+				continue;
+			}
+			if (number < 0){
+				System.out.println("Invalid number!");
+			} else {
 			todoList.markComplete(number);
+		}
 
 		} else if (command.equals("remove") || command.equals("r")) {
-			System.out.print("Item to remove: ");
-			int number = Integer.valueOf(scanner.nextLine());
-			todoList.remove(number);
+			todoList.print();
+			System.out.print("Item # to remove: ");
+			int number = 0;
+			if(scanner.hasNextInt()) {
+				 number = scanner.nextInt();
+			} else {
+				System.out.println("Please enter a number");
+				scanner.nextLine();
+				System.out.println();
+				continue;
+			}
+			if (number < 0){
+				System.out.println("Invalid number!");
+			} else {
+				todoList.remove(number);
+			}
 
 		} else if (command.equals("clear") || command.equals("c")) {
 			System.out.print("Are you sure you want to clear all items? Y/N? ");
@@ -53,6 +86,7 @@ public void start() {
 					todoList.remove(1);
 				}
 			}
+
 		} else if (command.equals("help") || command.equals("h")) {
 			System.out.println("Commands:");
 			System.out.println("(A)dd a task");
