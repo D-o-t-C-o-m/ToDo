a69b7f5c3b3a563f478a6276cb5da432b494cda6
D-o-t-C-o-m <valkuros@gmail.com>
2024-12-06 06:00:10 -0500

    Added help section, made shortcuts for commands, added clear and complete commands (complete not yet implemented)

--- TodoApp/To-Do App/src/UserInterface.java
+++ TodoApp/To-Do App/src/UserInterface.java
@@ -3,29 +3,62 @@ import java.util.Scanner;
 public class UserInterface {
 private Scanner scanner;
 private TodoList todoList;
-public UserInterface(TodoList todoList, Scanner scan){
+
+public UserInterface(TodoList todoList, Scanner scan) {
 	this.scanner = scan;
 	this.todoList = todoList;
 }
-public void start(){
-	System.out.println("Two Deux: Add to Add a Task, List to show a List of Tasks, Remove to remove a task, Stop to exit.");
+
+public void start() {
+	System.out.println("===To Deux===");
+	System.out.println("(H)elp for commands");
 	while (true) {
 		System.out.print("Command: ");
-		String command = scanner.nextLine();
-		if (command.equals("stop")) {
+		String command = scanner.nextLine().toLowerCase();
+		if (command.equals("stop") || command.equals("quit") || command.equals("q")) {
 			break;
-		} else if (command.equals("add")) {
-			System.out.print("To add: ");
+
+		} else if (command.equals("add") || command.equals("a")) {
+			System.out.print("Task to add: ");
 			String task = scanner.nextLine();
 			this.todoList.add(task);
-		}else if(command.equals("list")) {
-			todoList.print();
-		} else if (command.equals("remove")) {
-			System.out.print("Which one is removed: ");
+
+		} else if (command.equals("list") || command.equals("l")) {
+			if(todoList.size() == 0){
+				System.out.println("There are not tasks! (A)dd one now?");
+			} else {
+				todoList.print();
+			}
+		} else if (command.equals("mark") || command.equals("m")) {
+			System.out.print("Item to mark complete: ");
+			int number = Integer.valueOf(scanner.nextLine());
+
+		} else if (command.equals("remove") || command.equals("r")) {
+			System.out.print("Item to remove: ");
 			int number = Integer.valueOf(scanner.nextLine());
 			todoList.remove(number);
-			//TODO: Add a mark complete method that strikes out the text but doesn't remvove an item.
+
+		} else if (command.equals("clear") || command.equals("c")) {
+			System.out.print("Are you sure you want to clear all items? Y/N? ");
+			String confirm = scanner.nextLine().toLowerCase();
+			if (confirm.equals("yes") || confirm.equals("y")) {
+				while (todoList.size() > 0) {
+					todoList.remove(1);
+				}
+			}
+		} else if (command.equals("help") || command.equals("h")) {
+			System.out.println("Commands:");
+			System.out.println("(A)dd a task");
+			System.out.println("(M)ark a tasked completed");
+			System.out.println("(R)emove a task from the list");
+			System.out.println("(L)ist all tasks");
+			System.out.println("(C)lear all tasks from the list");
+			System.out.println("(Q)uit");
+
+		} else {
+			System.out.println("Invalid command");
 		}
 	}
 }
 }
+
