a263c104c4dfd43c77e07b8bb1236e41812f192c
D-o-t-C-o-m <valkuros@gmail.com>
2024-12-05 20:21:43 -0500

    Added Classes for a UI and the List itself. Basic POC now live.

---
+++ TodoApp/To-Do App/src/UserInterface.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class UserInterface {
+private Scanner scanner;
+private TodoList todoList;
+public UserInterface(TodoList todoList, Scanner scan){
+	this.scanner = scan;
+	this.todoList = todoList;
+}
+public void start(){
+	System.out.println("Two Deux: Add to Add a Task, List to show a List of Tasks, Remove to remove a task, Stop to exit.");
+	while (true) {
+		System.out.print("Command: ");
+		String command = scanner.nextLine();
+		if (command.equals("stop")) {
+			break;
+		} else if (command.equals("add")) {
+			System.out.print("To add: ");
+			String task = scanner.nextLine();
+			this.todoList.add(task);
+		}else if(command.equals("list")) {
+			todoList.print();
+		} else if (command.equals("remove")) {
+			System.out.print("Which one is removed: ");
+			int number = Integer.valueOf(scanner.nextLine());
+			todoList.remove(number);
+		}
+	}
+}
+}
