56f9fbfb8a5eb99cdac4219bd3eb3f653c0d99c8
D-o-t-C-o-m <valkuros@gmail.com>
2024-12-07 00:16:58 -0500

    Fixed possible crashes by entering too large of numbers, or non-text entires.

--- TodoApp/To-Do App/src/UserInterface.java
+++ TodoApp/To-Do App/src/UserInterface.java
@@ -4,9 +4,11 @@ public class UserInterface {
 private Scanner scanner;
 private TodoList todoList;
 
+
 public UserInterface(TodoList todoList, Scanner scan) {
 	this.scanner = scan;
 	this.todoList = todoList;
+
 }
 
 public void start() {
@@ -34,73 +36,65 @@ public void start() {
 			Utility utility = new Utility();
 			utility.createFile();
 
-	}
-
-	else if (command.equals("list") || command.equals("l")) {
-			if(todoList.size() == 0){
+		} else if (command.equals("list") || command.equals("l")) {
+			if (this.todoList.size() == 0) {
 				System.out.println("There are no tasks! (A)dd one now?");
 			} else {
-				todoList.print();
+				this.todoList.print();
 			}
 
 		} else if (command.equals("mark") || command.equals("m")) {
-				todoList.print();
+			this.todoList.print();
 			int number = 0;
-			if(scanner.hasNextInt()) {
+			System.out.print("Please enter a number: ");
+			if (this.scanner.hasNextInt()) {
 				number = scanner.nextInt();
-			} else {
-				System.out.println("Please enter a number");
-				scanner.nextLine();
-				System.out.println();
-				continue;
-			}
-			if (number < 0){
-				System.out.println("Invalid number!");
-			} else {
-			todoList.markComplete(number);
-		}
-
-		} else if (command.equals("remove") || command.equals("r")) {
-			todoList.print();
-			System.out.print("Item # to remove: ");
-			int number = 0;
-			if(scanner.hasNextInt()) {
-				 number = scanner.nextInt();
-			} else {
-				System.out.println("Please enter a number");
-				scanner.nextLine();
-				System.out.println();
-				continue;
-			}
-			if (number < 0){
-				System.out.println("Invalid number!");
-			} else {
-				todoList.remove(number);
-			}
-
-		} else if (command.equals("clear") || command.equals("c")) {
-			System.out.print("Are you sure you want to clear all items? Y/N? ");
-			String confirm = scanner.nextLine().toLowerCase();
-			if (confirm.equals("yes") || confirm.equals("y")) {
-				while (todoList.size() > 0) {
-					todoList.remove(1);
+				this.scanner.nextLine();
+				if (number <= 0 || number > this.todoList.size()) {
+					System.out.println("Invalid number!");
+				} else {
+					this.todoList.markComplete(number);
 				}
 			}
 
-		} else if (command.equals("help") || command.equals("h")) {
-			System.out.println("Commands:");
-			System.out.println("(A)dd a task");
-			System.out.println("(M)ark a tasked completed");
-			System.out.println("(R)emove a task from the list");
-			System.out.println("(L)ist all tasks");
-			System.out.println("(E)xport the task list");
-			System.out.println("(C)lear all tasks from the list");
-			System.out.println("(Q)uit");
+			} else if (command.equals("remove") || command.equals("r")) {
+				this.todoList.print();
+				int number = 0;
+				System.out.print("Item # to remove: ");
+				if (this.scanner.hasNextInt()) {
+					number = scanner.nextInt();
+					this.scanner.nextLine();
+				}
+				if (number <= 0 || number > this.todoList.size()) {
+					System.out.println("Invalid number!");
+				} else {
+					this.todoList.remove(number);
+				}
 
-		} else {
-			System.out.println("Invalid command");
+			} else if (command.equals("clear") || command.equals("c")) {
+				System.out.print("Are you sure you want to clear all items? Y/N? ");
+				String confirm = scanner.nextLine().toLowerCase();
+				if (confirm.equals("yes") || confirm.equals("y")) {
+					while (todoList.size() > 0) {
+						this.todoList.remove(1);
+					}
+				}
+
+			} else if (command.equals("help") || command.equals("h")) {
+				System.out.println("Commands:");
+				System.out.println("(A)dd a task");
+				System.out.println("(M)ark a tasked completed");
+				System.out.println("(R)emove a task from the list");
+				System.out.println("(L)ist all tasks");
+				System.out.println("(E)xport the task list");
+				System.out.println("(C)lear all tasks from the list");
+				System.out.println("(Q)uit");
+
+			} else {
+				System.out.println("Invalid command");
+			}
 		}
 	}
 }
-}
+
 
