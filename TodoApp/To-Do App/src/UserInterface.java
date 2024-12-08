929bababa26ac743623b954952fbe33788bb0116
D-o-t-C-o-m <valkuros@gmail.com>
2024-12-07 19:31:27 -0500

    Added a TODO into the notes around a future enhancement.

--- TodoApp/To-Do App/src/UserInterface.java
+++ TodoApp/To-Do App/src/UserInterface.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class UserInterface {
-private Scanner scanner;
-private TodoList todoList;
+private final Scanner scanner;
+private final TodoList todoList;
 
 
 public UserInterface(TodoList todoList, Scanner scan) {
@@ -15,53 +15,67 @@ public void start() {
 	System.out.println("===To Deux===");
 	System.out.println("To begin: (A)dd a task or (I)mport a previous List. (H)elp for commands");
 
+	label:
 	while (true) {
 
 		System.out.print("Command: ");
 		String command = scanner.nextLine().toLowerCase();
+//TODO: Create a command Class and use a switch/case to move all this code out of the UI
+		switch (command) {
+			case "stop":
+			case "quit":
+			case "q":
+			case "exit":
+				break label;
 
-		if (command.equals("stop") || command.equals("quit") || command.equals("q") || command.equals("exit")) {
-			break;
+			case "add":
+			case "a":
+				System.out.print("Task to add (!B to go cancel): ");
+				String task = scanner.nextLine();
+				if (task.equals("!B") || task.equals("!b")) {
+					System.out.println();
+				} else {
+					this.todoList.add(task);
+				}
 
-		} else if (command.equals("add") || command.equals("a")) {
-			System.out.print("Task to add (!B to go cancel): ");
-			String task = scanner.nextLine();
-			if (task.equals("!B") || task.equals("!b")) {
-				System.out.println();
-			} else {
-				this.todoList.add(task);
-			}
+				break;
+			case "export":
+			case "e":
+				Utility utility = new Utility();
+				utility.createFile(this.todoList);
 
-		} else if (command.equals("export") || command.equals("e")) {
-			Utility utility = new Utility();
-			utility.createFile(this.todoList);
+				break;
+			case "list":
+			case "l":
+				if (this.todoList.size() == 0) {
+					System.out.println("There are no tasks! (A)dd one now?");
+				} else {
+					this.todoList.print();
+				}
 
-		} else if (command.equals("list") || command.equals("l")) {
-			if (this.todoList.size() == 0) {
-				System.out.println("There are no tasks! (A)dd one now?");
-			} else {
-				this.todoList.print();
-			}
-
-		} else if (command.equals("mark") || command.equals("m")) {
-			if (this.todoList.size() == 0) {
-				System.out.println("There are no tasks! (A)dd one now?");
-			}else {
-				this.todoList.print();
-				int number = 0;
-				System.out.print("Please enter a number: ");
-				if (this.scanner.hasNextInt()) {
-					number = scanner.nextInt();
-					this.scanner.nextLine();
-					if (number <= 0 || number > this.todoList.size()) {
-						System.out.println("Invalid number!");
-					} else {
-						this.todoList.markComplete(number);
+				break;
+			case "mark":
+			case "m":
+				if (this.todoList.size() == 0) {
+					System.out.println("There are no tasks! (A)dd one now?");
+				} else {
+					this.todoList.print();
+					int number = 0;
+					System.out.print("Please enter a number: ");
+					if (this.scanner.hasNextInt()) {
+						number = scanner.nextInt();
+						this.scanner.nextLine();
+						if (number <= 0 || number > this.todoList.size()) {
+							System.out.println("Invalid number!");
+						} else {
+							this.todoList.markComplete(number);
+						}
 					}
 				}
-			}
 
-			} else if (command.equals("remove") || command.equals("r")) {
+				break;
+			case "remove":
+			case "r":
 				this.todoList.print();
 				int number = 0;
 				System.out.print("Item # to remove: ");
@@ -75,7 +89,9 @@ public void start() {
 					this.todoList.remove(number);
 				}
 
-			} else if (command.equals("clear") || command.equals("c")) {
+				break;
+			case "clear":
+			case "c":
 				System.out.print("Are you sure you want to clear all items? Y/N? ");
 				String confirm = scanner.nextLine().toLowerCase();
 				if (confirm.equals("yes") || confirm.equals("y")) {
@@ -84,7 +100,9 @@ public void start() {
 					}
 				}
 
-			} else if (command.equals("help") || command.equals("h")) {
+				break;
+			case "help":
+			case "h":
 				System.out.println("Commands:");
 				System.out.println("(A)dd a task");
 				System.out.println("(M)ark a tasked completed");
@@ -94,11 +112,13 @@ public void start() {
 				System.out.println("(C)lear all tasks from the list");
 				System.out.println("(Q)uit");
 
-			} else {
+				break;
+			default:
 				System.out.println("Invalid command");
-			}
+				break;
 		}
-	}
+		}
+}
 }
 
 
